73d65f2f4b728ef7b9e14e99f69b8bab717da72b
dhiooawp <dhio.anugerah.ft21@mail.umy.ac.id>
2023-11-08 23:08:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/navigationwithdata/data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.navigationwithdata.data
+
+import com.example.navigationwithdata.R
+
+object SumberData {
+    val flavour = listOf(
+        R.string.natural,
+        R.string.vanilla,
+        R.string.lyche,
+        R.string.milk,
+        R.string.lemon
+    )
+}
