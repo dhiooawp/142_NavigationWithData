dc1d4ab9a4d6943b17b51ccf1d4bc6254975251a
dhiooawp <dhio.anugerah.ft21@mail.umy.ac.id>
2023-11-16 01:56:07 +0700

    Menambahkan OutlinedText untuk nama, alamat, dan nomor telpon di dalam Column pada HalamanForm.kt

--- app/src/main/java/com/example/navigationwithdata/HalamanForm.kt
+++ app/src/main/java/com/example/navigationwithdata/HalamanForm.kt
@@ -1,8 +1,13 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.navigationwithdata
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -10,6 +15,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 
 @Composable
 fun HalamanForm(
@@ -26,7 +32,15 @@ fun HalamanForm(
         verticalArrangement = Arrangement.Center,
         modifier = Modifier.fillMaxWidth()
     ){
-
+        OutlinedTextField(value = namaTxt, onValueChange = {namaTxt = it}, label = {
+            Text(text = stringResource(id = R.string.nama))
+        })
+        OutlinedTextField(value = alamatTxt, onValueChange = {alamatTxt = it}, label = {
+            Text(text = stringResource(id = R.string.alamat))
+        })
+        OutlinedTextField(value = tlpnTxt, onValueChange = {tlpnTxt = it}, label = {
+            Text(text = stringResource(id = R.string.nomor))
+        })
     }
 
 }
