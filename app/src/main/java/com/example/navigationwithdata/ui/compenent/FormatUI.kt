e42278146bfc093b11a4ac0155f27de90896e012
dhiooawp <dhio.anugerah.ft21@mail.umy.ac.id>
2023-11-16 02:42:35 +0700

    Menambahkan FormatDataPemesanan di dalam FormatUI.ktt

--- app/src/main/java/com/example/navigationwithdata/ui/compenent/FormatUI.kt
+++ app/src/main/java/com/example/navigationwithdata/ui/compenent/FormatUI.kt
@@ -1,10 +1,14 @@
 package com.example.navigationwithdata.ui.compenent
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.navigationwithdata.R
 
 @Composable
@@ -14,4 +18,35 @@ fun FormatLabelHarga(subtotal: String, modifier: Modifier = Modifier) {
         modifier = modifier,
         style = MaterialTheme.typography.headlineSmall
     )
+}
+
+@Composable
+fun FormatDataPemesanan(
+    namaPemesanan: String,
+    alamatPemesan: String,
+    phonePemesan: String,
+){
+    Text(
+        text = stringResource(R.string.nama),
+    )
+    Text(namaPemesanan)
+    Divider()
+    Spacer(modifier = Modifier.padding(8.dp))
+
+
+    Text(
+        text = stringResource(R.string.alamat),
+    )
+    Text(alamatPemesan)
+    Divider()
+    Spacer(modifier = Modifier.padding(10.dp))
+
+
+    Text(
+        text = stringResource(R.string.nomor),
+    )
+    Text(phonePemesan)
+    Divider()
+
+
 }
