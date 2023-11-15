7ad541706f8469f987aebe71f8f02da528fa2309
dhiooawp <dhio.anugerah.ft21@mail.umy.ac.id>
2023-11-16 01:42:57 +0700

    menambahkan variable nama, alamat, dan notlp di data class OrderUiState

--- app/src/main/java/com/example/navigationwithdata/data/OrderUiState.kt
+++ app/src/main/java/com/example/navigationwithdata/data/OrderUiState.kt
@@ -4,4 +4,7 @@ data class OrderUiState(
     val jumlah: Int = 0,
     val rasa: String ="",
     val harga: String ="",
+    val nama: String = "",
+    val alamat: String = "",
+    val tlp: String = ""
 )
